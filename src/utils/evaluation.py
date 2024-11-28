a9f02439bb3d287a1d18911bae3e944f9255e121
tinymonster123 <9109223208@email.ncu.edu.cn>
2024-11-28 21:39:51 +0800

    chore : 更新评估模型的指标为 MAF 和 WAF

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -1,11 +1,13 @@
 import torch
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report,f1_score
 import numpy as np
 import pandas as pd
 
 def evaluate_model(classifier, X_test, y_test, device):
     classifier.eval()
-    with torch.no_grad():
+    with torch.no_grad(): # 禁止使用梯度计算
+        
+        # 将 X_test 和 y_test 转换为张量
         if isinstance(X_test, torch.Tensor):
             X_test = X_test.to(device)
         elif isinstance(X_test, pd.DataFrame) or isinstance(X_test, pd.Series):
@@ -24,9 +26,16 @@ def evaluate_model(classifier, X_test, y_test, device):
         else:
             raise TypeError(f"Unsupported type for y_test: {type(y_test)}")
 
+        # 使用模型进行预测
         outputs = classifier(X_test)
         _, preds = torch.max(outputs, 1)
         preds = preds.cpu().numpy()
         y_true = y_test.cpu().numpy()
+        
+        maf = f1_score(y_true, preds, average='macro',zero_division=0)
+        waf = f1_score(y_true, preds, average='weighted',zero_division=0)
+        
         report = classification_report(y_true, preds, zero_division=0)
         print("Evaluation Report:\n", report)
+        print(f"Macro F1 Score: {maf:.4f}")
+        print(f"Weighted F1 Score: {waf:.4f}")
