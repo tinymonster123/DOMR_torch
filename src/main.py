60446a8174f0a5a078881cc16fd385ca74e74522
tinymonster123 <9109223208@email.ncu.edu.cn>
2024-11-28 21:44:31 +0800

    fix : 优化 dataset 文件中参数传递问题以及添加将数据集划分为已知数据和未知数据集的功能

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ from models.representation import Representation
 from models.meta_learning import MAML, train_maml_model
 from models.classifier import Classifier, train_classifier
 from utils.evaluation import evaluate_model
-from utils.visualization import plot_bar_charts, plot_line_charts, plot_heatmap, plot_sample_ratio_line_chart
 from utils.feature_extraction import extract_features
 
 
@@ -22,8 +21,9 @@ def main():
     inner_steps = 5
     meta_iterations = 10
     classifier_epochs = 10
+    N = 7
 
-    X_train, X_test, y_train, y_test = load_processed_data(file_name, raw_data_dir, processed_data_dir)
+    X_train, X_test, y_train, y_test,known_classes = load_processed_data(file_name, raw_data_dir, processed_data_dir,N)
 
     train_loader, test_loader = create_dataloaders(X_train, y_train, X_test, y_test, batch_size=batch_size)
 
@@ -53,70 +53,6 @@ def main():
     torch.save(classifier.state_dict(), 'src/models/classifier.pth')
 
     evaluate_model(classifier, test_features, test_labels, device)
-
-    experiment_data = pd.DataFrame({
-        'New Family Count': [1, 1, 2, 2, 3, 3],
-        'Method': ['DOMR', 'EVM', 'DOMR', 'EVM', 'DOMR', 'EVM'],
-        'WAF': [75, 70, 80, 72, 85, 78],
-        'MAF': [70, 68, 75, 70, 80, 74]
-    })
-
-    plot_bar_charts(
-        data=experiment_data,
-        x='New Family Count',
-        y='WAF',
-        hue='Method',
-        title='Comparison of WAF Performance of DOMR vs EVM under Different New Family Counts',
-        xlabel='New Family Count',
-        ylabel='WAF Performance',
-        save_path='experiments/plots/experiment_waf_bar_chart.png'
-    )
-
-    plot_bar_charts(
-        data=experiment_data,
-        x='New Family Count',
-        y='MAF',
-        hue='Method',
-        title='Comparison of MAF Performance of DOMR vs EVM under Different New Family Counts',
-        xlabel='New Family Count',
-        ylabel='MAF Performance',
-        save_path='/experiments/plots/experiment_maf_bar_chart.png'
-    )
-
-    parameter_data = pd.DataFrame({
-        'λ1': [0.1, 0.1, 0.2, 0.2],
-        'λ2': [0.01, 0.02, 0.01, 0.02],
-        'MAF': [75.5, 76.0, 78.2, 79.1]
-    })
-
-    plot_heatmap(
-        data=parameter_data,
-        x='λ1',
-        y='λ2',
-        z='MAF',
-        title='Comparison of MAF Performance under Different λ1 and λ2 Values',
-        xlabel='λ1',
-        ylabel='λ2',
-        save_path='/experiments/plots/parameter_heatmap.png'
-    )
-
-    sample_ratio_data = pd.DataFrame({
-        'NETWORK_ACCESS____': [0.1, 0.2, 0.3, 0.4, 0.5],
-        'Known Family Classification MAF': [70.2, 72.5, 75.0, 77.3, 78.9],
-        'Open Recognition MAF': [65.4, 67.8, 70.1, 72.5, 74.0]
-    })
-
-    plot_sample_ratio_line_chart(
-        data=sample_ratio_data,
-        x='NETWORK_ACCESS____',
-        y1='Known Family Classification MAF',
-        y2='Open Recognition MAF',
-        title='Comparison of MAF Performance under Different Sample Ratios',
-        xlabel='NETWORK_ACCESS____',
-        ylabel1='Known Family Classification MAF',
-        ylabel2='Open Recognition MAF',
-        save_path='experiments/plots/sample_ratio_line_chart.png'
-    )
 
 
 if __name__ == '__main__':
--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,23 +1,47 @@
 import os
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
 import torch
 
+# 加载原始数据集
 def dataset_loader(raw_data_dir,file_name):
     data_path = os.path.join(raw_data_dir, file_name)
     data = pd.read_csv(data_path)
-    return data
+    return data # 返回数据集为 DataFrame 对象
 
 def preprocess_data(data):
-    x = data.drop(columns=['Class'])
-    y = data['Class']
+    x = data.drop(columns=['Class']) # 返回 x 为特征数据(去除 Class 列)
+    y = data['Class'] # 返回 y 为标签数据(Class)
     return x,y
 
+def find_known_class(N,y): # N 为已知类别的数量,且 N 的取值范围为 {7,9,11,13}
+    unique_labels = y.unique()
+    known_classes = np.random.choice(unique_labels, N, replace=False)
+    return known_classes # 返回 N 个已知类别 根据论文的说法作为已知类别的同时，也是旧类别
+        
 
-def split_data(x,y):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-    return x_train, x_test, y_train, y_test
+def split_data(x,y,N,test_size = 1/6):
+    
+    known_classes = find_known_class(N,y) 
+    known_mask = y.isin(known_classes) # 同理
+    unknown_mask = ~known_mask # 创建布尔掩码用于表示未知类别和已知样本
+    
+    x_known = x[known_mask]
+    y_known = y[known_mask]
+    
+    x_unknown = x[unknown_mask]
+    y_unknown = y[unknown_mask]
+    
+    # 根据论文的比例来对已知类别数据进行分类为——训练集和测试集
+    x_train, x_test, y_train, y_test = train_test_split(x_known, y_known, test_size=test_size, random_state=42, stratify=y_known)
+    
+    # 将未知类别数据添加到测试集中
+    x_test = pd.concat([x_test, x_unknown])
+    y_test = pd.concat([y_test, y_unknown])
+    return x_train, x_test, y_train, y_test,known_classes
+    
 
 def create_episode(X_train, y_train, num_episode=100):
     episodes = []
@@ -39,13 +63,13 @@ def make_processed_dataset(x_train,x_test,y_train,y_test,processed_data_dir):
     train_data.to_csv(os.path.join(processed_data_dir, 'train.csv'), index=False)
     test_data.to_csv(os.path.join(processed_data_dir, 'test.csv'), index=False)
     
-def load_processed_data(file_name,raw_data_dir,processed_data_dir):
+def load_processed_data(file_name,raw_data_dir,processed_data_dir,N):
     data = dataset_loader(raw_data_dir,file_name)
     x,y = preprocess_data(data)
-    x_train, x_test, y_train, y_test = split_data(x,y)
+    x_train, x_test, y_train, y_test,known_classes = split_data(x,y,N)
     make_processed_dataset(x_train,x_test,y_train,y_test,processed_data_dir)
     
-    return (x_train, x_test, y_train, y_test)
+    return (x_train, x_test, y_train, y_test,known_classes)
 
 def create_dataloaders(X_train, y_train, X_test, y_test, batch_size):
     train_dataset = TensorDataset(torch.tensor(X_train.values, dtype=torch.float32),
